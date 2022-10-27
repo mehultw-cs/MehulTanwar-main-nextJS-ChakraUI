5ad9cb7e5329184f3ecb4e0d31180669e828dd32
Mehul Tanwar <mehultw.cs@gmail.com>
2022-10-26 23:52:05 -0400

    updated stack-data

--- components/Stack.data.js
+++ components/Stack.data.js
@@ -23,7 +23,7 @@ export default [
   // },
   {
     label: 'CloudFlare',
-    image:"https://cdn.cdnlogo.com/logos/c/5/cloudflare.svg",
+    image: 'https://cdn.cdnlogo.com/logos/c/5/cloudflare.svg',
     link: 'https://www.cloudflare.com/',
   },
   {
@@ -33,9 +33,9 @@ export default [
     link: 'https://code.visualstudio.com/',
   },
   {
-    label: 'Vercel',
-    image: 'https://cdn.cdnlogo.com/logos/v/78/vercel.svg',
-    link: 'https://vercel.com/',
+    label: 'Render',
+    image: 'https://avatars.githubusercontent.com/u/36424661?s=200&v=4',
+    link: 'https://render.com/',
   },
   // {
   //   label: 'OracleCloud',
@@ -47,5 +47,11 @@ export default [
     image:
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg',
     link: 'https://nodejs.org/',
+  },
+  {
+    label: 'Framer Motion',
+    image:
+      'https://camo.githubusercontent.com/179d66ab2b0321726c88a586c4ad38802e7113a3c98c6fd3f0156c01c98cfd14/68747470733a2f2f6672616d657275736572636f6e74656e742e636f6d2f696d616765732f34386861395a52396f5a51475136675a38595566456c50335430412e706e67',
+    link: 'https://www.framer.com/motion/',
   },
 ];
